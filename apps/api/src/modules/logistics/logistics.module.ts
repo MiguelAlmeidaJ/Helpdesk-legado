3c1479ce905f60b09d78c2a1fd0b446e0fd29413
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-21 15:37:25 -0300

    feat: habilita modulo financeiro

--- apps/api/src/modules/logistics/logistics.module.ts
+++ apps/api/src/modules/logistics/logistics.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ExpensesModule } from './expenses/expenses.module';
+import { FinanceModule } from './finance/finance.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [ExpensesModule, VehiclesModule],
+  imports: [ExpensesModule, FinanceModule, VehiclesModule],
 })
 export class LogisticsModule {}
