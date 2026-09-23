b820421a689e54d1aa4a3e6aea9e4c92eb80980f
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-23 10:17:36 -0300

    refactor: remove criacao local do banco n3rd

--- docker/mysql/init/00-create-databases.sql
+++ docker/mysql/init/00-create-databases.sql
@@ -1,7 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `nivel3`
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
-
-CREATE DATABASE IF NOT EXISTS `n3rd`
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
